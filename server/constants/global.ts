d4bdfa4e8b2d4fb0c461ee6b03482f15a8011279
Aleks <aleksandar.trpkovski@gmail.com>
2022-01-26 17:13:18 +1100

    now return image url as well

--- server/constants/global.ts
+++ server/constants/global.ts
@@ -1,2 +1,3 @@
 export const EXCLUDE_FILES = ['README.md', 'TABLE-OF-CONTENTS.MD']
 export const TABLE_OF_CONTENT = 'TABLE-OF-CONTENTS.MD'
+export const GITHUB_IMAGE_URL = 'https://raw.githubusercontent.com/Suv4o/wallpaper-images/master/'
--- server/utils/getImageData.ts
+++ server/utils/getImageData.ts
@@ -1,14 +1,17 @@
 import { RepoData } from '../../interfaces/server'
 import getImageDescription from './getImageDescription'
-import { EXCLUDE_FILES } from '../constants/global'
+import { EXCLUDE_FILES, GITHUB_IMAGE_URL } from '../constants/global'
 
 export default function getImageData(data: RepoData): Array<object> {
     const result = data.data.user.repository.object.entries
         .map((entry) => {
             if (!EXCLUDE_FILES.includes(entry.name)) {
                 const imageId = entry.name.split('-', 1)[0]
-
-                return { ...entry, ...{ description: getImageDescription(data, imageId) } }
+                return {
+                    ...entry,
+                    ...{ url: `${GITHUB_IMAGE_URL + entry.name}` },
+                    ...{ description: getImageDescription(data, imageId) }
+                }
             }
             return entry
         })

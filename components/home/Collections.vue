8cc93bfffd51f60bf3244833b23f320080299e91
Aleks <aleksandar.trpkovski@gmail.com>
2022-02-05 14:49:15 +1100

    Added placeholder images on load On mounted in Collections component, if images are not loaded don't render the image. Instead, render leaves a grayish placeholder background.

--- components/home/Collections.vue
+++ components/home/Collections.vue
@@ -5,21 +5,24 @@
                 <div class="space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
                     <div v-for="image in images" :key="image.name" class="group relative">
                         <div
-                            class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1"
+                            class="relative w-full h-80 bg-slate-300 rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1"
                         >
                             <img
+                                v-if="image.smallUrl"
                                 :src="image.smallUrl"
-                                :alt="image.description.Name"
+                                :alt="image.description ? image.description.Name : ''"
                                 class="w-full h-full object-center object-cover"
                             />
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
                             <a :href="image.smallUrl">
                                 <span class="absolute inset-0" />
-                                {{ image.description.Author }}
+                                {{ image.description ? image.description.Author : '' }}
                             </a>
                         </h3>
-                        <p class="text-base font-semibold text-gray-900 mb-6">{{ image.description.Name }}</p>
+                        <p class="text-base font-semibold text-gray-900 mb-6">
+                            {{ image.description ? image.description.Name : '' }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -28,18 +31,38 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { ImageFromApi } from '@/interfaces/client'
 import { Entries } from '@/interfaces/server'
 let images = ref<Entries[]>([])
+let search = ref<string>('')
+let page = 1
 
 onMounted(async () => {
+    images.value = fillPlaceholderImages(
+        {
+            name: '',
+            extension: '',
+            object: {
+                byteSize: 0,
+                text: ''
+            }
+        },
+        18
+    )
     const response = await getData()
     images.value = response.collection
 })
 
-async function getData(): Promise<ImageFromApi> {
-    return await $fetch('/api/get-images?size=18&page=1')
+async function getData(size: number = 18, page: number = 1, search: string = ''): Promise<ImageFromApi> {
+    if (search) {
+        return await $fetch(`/api/get-images?size=${size}&page=${1}&search=${search}`)
+    }
+    return await $fetch(`/api/get-images?size=${size}&page=${1}`)
+}
+
+function fillPlaceholderImages(value: Entries, len: number): Entries[] {
+    return [...Array(len).keys()].map(() => value)
 }
 </script>
 
